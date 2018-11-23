f6b38e388a88019789fbd4564082df07fd0518f7
Plínio Naves <pliniopjn@gmail.com>
2018-11-23 15:40:44 -0200

    Agrupando dados e setando valor inicial nos inputs

--- src/App.vue
+++ src/App.vue
@@ -25,17 +25,25 @@
                 type="text" 
                 class="form-control" 
                 placeholder="Seu nome"
-                v-model="nome">
+                v-model="desenvolvedor.nome">
             </div>
 
             <div class="form-group">
               <label>Endereço de email:</label>
-              <input type="email" class="form-control" placeholder="Seu email">
+              <input 
+                type="email" 
+                class="form-control" 
+                placeholder="Seu email"
+                v-model="desenvolvedor.email">
             </div>
 
             <div class="form-group">
               <label>Idade:</label>
-              <input type="number" class="form-control" placeholder="Sua idade">
+              <input 
+                type="number" 
+                class="form-control" 
+                placeholder="Sua idade"
+                v-model="desenvolvedor.idade">
             </div>
 
             <div class="form-group">
@@ -118,9 +126,9 @@
             <div class="card-header">Dados</div>
 
             <ul class="list-group list-group-flush">
-              <li class="list-group-item"><strong>Nome:</strong> {{ nome }}</li>
-              <li class="list-group-item"><strong>Email:</strong> </li>
-              <li class="list-group-item"><strong>Idade:</strong></li>
+              <li class="list-group-item"><strong>Nome:</strong> {{ desenvolvedor.nome }}</li>
+              <li class="list-group-item"><strong>Email:</strong> {{ desenvolvedor.email }}</li>
+              <li class="list-group-item"><strong>Idade:</strong> {{ desenvolvedor.idade }}</li>
               <li class="list-group-item"><strong>Gênero:</strong> </li>
               <li class="list-group-item"><strong>Ocupação:</strong> </li>
               <li class="list-group-item"><strong>Tecnologias:</strong> </li>
@@ -131,7 +139,7 @@
             <div class="card-header">Model</div>
 
             <div class="card-body">
-              <pre><code>{{ {'nome': 'Plínio Naves'} }}</code></pre>
+              <pre><code>{{ desenvolvedor }}</code></pre>
             </div>
 
           </div>
@@ -149,7 +157,11 @@
 export default {
   data() {
     return {
-      nome: ''
+      desenvolvedor: {
+        nome: 'Plínio',
+        email: '',
+        idade: 28
+      }
     }
   }
 }
